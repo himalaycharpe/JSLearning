f06101c3521a86b76434fb01e7c520d2ea9b3eb2
himalaycharpe <himalaycharpe@gmail.com>
2023-02-16 13:10:58 +0530

    firstday of javascript
    firstday of javascript  hello world

---
+++ 01_variable_assign.js
@@ -0,0 +1,32 @@
+console.log("----- step 1 -----");
+var myFullName;    // before initialazation
+console.log(myFullName);
+
+var myFullName ="Himalay Charpe";  //after initialisation 
+console.log(myFullName);
+
+var myFullName ="Himalay Chandrashekhar Charpe";
+console.log(myFullName);
+
+console.log("----- step 2 -----");
+var myLove ="i love javaScript";
+console.log(myLove);
+
+console.log("----- step 3 -----");
+var firstName = "Himalay";
+console.log("First Name:",firstName);
+var lastName = "Charpe";
+console.log("Last Name:",lastName);
+var age = "27";
+console.log("Age:",age);
+var address = "Hinjewadi, Pune";
+console.log("Address:",address);
+var pincode = "411057";
+console.log("Pin Code:",pincode);
+var gender = "Male";
+console.log("Gender:",gender);
+var isMarried = "No";
+console.log("Maretial Status:",isMarried);
+console.log("-------------------------");
+console.log("First_name:",firstName,    "Last_name:",lastName, "Address:",address);
+
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,24 @@
 console.log(" Himalay Charpe ");
+console.log("31");
+console.log("pune");
+
+var fullName = "Himalay Charpe";
+console.log(fullName);
+console.log(fullName);
+
+var age = "26"; // variable declaation & initialazation in same line 
+age ="27";
+age ="28";
+console.log(age);
+
+var universityName = "RTMNU"; //variable initialation
+console.log(universityName);
+console.log("my university name is ", universityName);
+
+console.log("*****1******");
+console.log("i love JS");
+
+console.log("******2******");
+console.log("full name is ");
+
+console.log("*****3*******");
